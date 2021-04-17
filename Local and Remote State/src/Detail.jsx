086b9f828ca42a0214f7f90350373a055bf9ffa5
Douglas Chalegre <douglas.chalegre@gmail.com>
2021-04-16 23:43:58 -0300

    💹 Redirecting via useNavigation

--- Local and Remote State/src/Detail.jsx
+++ Local and Remote State/src/Detail.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './services/useFetch';
 import PageNotFound from './PageNotFound';
 import Spinner from './Spinner';
 
 export default function Detail() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { data: product, loading, error } = useFetch('products/' + id);
 
   if (loading) return <Spinner />;
@@ -17,6 +18,11 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id='price'>${product.price}</p>
+      <p>
+        <button className='btn btn-primary' onClick={() => navigate('/cart')}>
+          Add to cart
+        </button>
+      </p>
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
